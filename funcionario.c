001a4dd63f7cf4888b69450750b27b060ff0df3c
Paulo Henrique <138056303+Oluaphenrique@users.noreply.github.com>
2023-08-23 20:22:24 +0000

    add

---
+++ funcionario.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct funcionario{
+
+char nome[51];
+char cargo[11];
+int documento;
+
+};
+
+    Funcionario * criarfuncionario(char * nome, char * cargo, int documento, ){
+    Funcionario * criar = (Funcionario*) malloc(sizeof(Funcionario));
+    
+    if (conta == NULL){
+    exit(1);
+}
+
+
+return criar;
+}
+
