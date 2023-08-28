c1b0e2e439c308704d5d000d90df9d147d4d1dbf
diegonascimento2023 <diego.oliveira24023@alunos.ufersa.edu.br>
2023-08-28 14:02:28 -0300

    Funçao cria funcionario

--- funcionario.c
+++ funcionario.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct funcionario{
+typedef struct funcionario
+{
 
-char nome[51];
-char cargo[11];
-int documento;
+    char nome[51];
+    char cargo[11];
+    int documento;
 
-};
+} Funcionario;
 
-    Funcionario * criarFuncionario(char * nome, char * cargo, int documento, ){
-    Funcionario * criar = (Funcionario*) malloc(sizeof(Funcionario));
-    
-    if (conta == NULL){
-    exit(1);
+Funcionario *criarFuncionario(char *nome, char *cargo, int documento)
+{
+    Funcionario *funcionario = (Funcionario *)malloc(sizeof(Funcionario));
+
+    if (funcionario == NULL)
+    {
+        exit(1);
+    }
+
+    strcpy(funcionario->nome, nome);
+    strcpy(funcionario->cargo, cargo);
+    funcionario->documento = documento;
+
+    return funcionario;
 }
 
+void funcionario_salva(FILE *fp, Funcionario *funcionario)
+{
 
-return criar;
+    // Abrir/criar o arquivo de saida para escrita (w)
+    fp = fopen("saida_q3.txt", "w");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        exit(1);
+    }
 
+   
+
+        fprintf(fp, "Funcionario\tDocumento\tCargo\t\n%s\t%d\t%s", funcionario->nome, funcionario->documento, funcionario->cargo);
+    
 }
 
--- funcionario.h
+++ funcionario.h
@@ -2,10 +2,8 @@
 
 /* Vai pedir para usuario inserir nome, cargo e documento */
 
-typedef struct funcionario Funcionario{
+typedef struct funcionario Funcionario;
 
+Funcionario *criarFuncionario(char *nome, char *cargo, int documento);
 
-    Funcionario * criarFuncionario(char * nome, char * cargo, int documento);
-
-
-}
+void funcionario_salva(FILE *fp, Funcionario *funcionario);
